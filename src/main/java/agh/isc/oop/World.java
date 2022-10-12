1773910866009f54b6844e46af285089ae267f5a
marti3 <marcioch.s.m@gmail.com>
2022-10-12 11:08:33 +0200

    pierwsza wersja programu

--- src/main/java/agh/isc/oop/World.java
+++ src/main/java/agh/isc/oop/World.java
@@ -5,28 +5,20 @@ import static agh.isc.oop.Direction.*;
 public class World {
     public static void main(String[] args){
     System.out.println ("system rozpoczął działanie");
-    // Direction [] tablica = {f, f, f, r, l, r};
-    // run(tablica);
+    Direction [] tablica = {f, f, f, r, l, r};
+    run(tablica);
     System.out.println ("System zakończył działanie");
     }
     public static void run(Direction [] args) {
-        int n=args.length;
         //System.out.println("zwierzak idzie do przodu");
-        for (int i=0; i<n; i++) {
-            switch (args[i]) {
-                case f:
-                    System.out.println("zwierzak idzie do przodu");
-                    break;
-                case b:
-                    System.out.println("zwierzak idzie do tyłu");
-                    break;
-                case r:
-                    System.out.println("zwierzak skreca w prawo");
-                    break;
-                case l:
-                    System.out.println("zwierzak skreca w lewo");
-                    break;
-            }
+        for (Direction argument : args) {
+            String message = switch (argument) {
+                case f -> "zwierzak idzie do przodu";
+                case b -> "zwierzak idzie do tyłu";
+                case r ->"zwierzak skreca w prawo";
+                case l -> "zwierzak skreca w lewo";
+            };
+            System.out.println(message);
         }
     }
 }
